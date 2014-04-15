f6b3853a66475c458e517745ba70b173268b378d
ympons <yaismelmp@googlemail.com>
2014-04-15 11:50:24 -0400

    Added web files and folder

---
+++ web/js/basiclti.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".basiclti_launch").fancybox({
+    'width'         : '100%',
+    'height'        : '100%',
+    'autoScale'     : true,
+//    'opacity'       : true,
+//    'transitionIn'  : 'elastic',
+//    'transitionOut' : 'elastic',
+    'type'          : 'iframe'
+  });
+})
